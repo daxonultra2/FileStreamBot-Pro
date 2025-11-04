2eacf10dd1da8261fdefb0b4b35eb3b8b996530f
daxonultra2 <daxonultra2@gmail.com>
2025-11-04 19:17:02 +0530

    Update stream.py

--- ShivamNox/bot/plugins/stream.py
+++ ShivamNox/bot/plugins/stream.py
@@ -101,18 +101,19 @@ async def private_receive_handler(c: Client, m: Message):
 
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = f"{Var.URL}watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
-        online_link = f"{Var.URL}dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        stream_link = f"http://hivajoy.fwh.is/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        online_link = f"http://hivajoy.fwh.is/dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        stream_link2 = f"https://filestreambot-pro-ad54.onrender.com/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
         
-        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥WATCH  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : LINK WILL NEVER EXPIRE</b>"""
+        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥WATCH  :</b> <i>{}</i>\n\n<b> 🖥Stream 2  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : LINK WILL NEVER EXPIRE</b>"""
         
         await log_msg.reply_text(
-            text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}",
+            text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}\n**Stream ʟɪɴᴋ :** {stream_link2}",
             disable_web_page_preview=True,
             quote=True
         )
         await m.reply_text(
-            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
+            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link, stream_link2),
             quote=True,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -152,11 +153,12 @@ async def channel_receive_handler(bot, broadcast):
     
     try:
         log_msg = await broadcast.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = f"{Var.URL}watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
-        online_link = f"{Var.URL}dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        stream_link = f"http://hivajoy.fwh.is/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        online_link = f"http://hivajoy.fwh.is/dl/{str(log_msg.id)}?hash={get_hash(log_msg)}"
+        stream_link2 = f"https://filestreambot-pro-ad54.onrender.com/watch/{str(log_msg.id)}?hash={get_hash(log_msg)}"
         
         await log_msg.reply_text(
-            text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link}",
+            text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link}\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link2}",
             quote=True
         )
         await bot.edit_message_reply_markup(
